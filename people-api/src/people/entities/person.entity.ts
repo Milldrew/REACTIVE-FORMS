71207ec37cfcbb56326f458ad813b7f4960f477a
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-03-31 16:53:00 -0600

    📅  | Thu Mar 31 16:53:00 MDT 2022

--- people-api/src/people/entities/person.entity.ts
+++ people-api/src/people/entities/person.entity.ts
@@ -1,1 +1,11 @@
-export class Person {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Person {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  firstName: string;
+  @Column()
+  lastName: string;
+}
