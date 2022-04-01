c4fca44f174bb61a071d3aed8411f3c30635c35a
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-01 11:59:29 -0600

    📅  | Fri Apr  1 11:59:29 MDT 2022

--- reactive-forms-tutorial/src/app/services/create-read-delete.service.ts
+++ reactive-forms-tutorial/src/app/services/create-read-delete.service.ts
@@ -1,15 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CreateReadDeleteService {
+  people: any;
   constructor(private http: HttpClient) {}
-  createUser() {
-    console.log('creaet user');
+  getPeople() {
     this.http
       .get('http://192.168.0.143:3000/people')
-      .subscribe((value) => console.log(value));
+      .subscribe((value) => (this.people = value));
+  }
+  createPerson() {
+    console.log('hi');
+    this.http
+      .get('http://192.168.0.143:3000/people')
+      .pipe(tap((data) => console.log(data)));
   }
 }
